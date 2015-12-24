a78b575737edd619c677738d23fe68ab43586394
nicojs <jansennico@gmail.com>
2015-12-24 13:51:19 +0100

    Created a package which can be required with nodejs

---
+++ src/html2commonmark.ts
@@ -0,0 +1,6 @@
+import JSDomConverter = require('./JSDomConverter');
+import Converter = require('./Converter');
+import MarkdownRenderer = require('./MarkdownRenderer');
+
+
+export = { JSDomConverter, Converter, Renderer: MarkdownRenderer };
